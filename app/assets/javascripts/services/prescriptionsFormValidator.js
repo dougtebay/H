6bf77fb19be84744db01823bec4971a68af3b354
dougtebay <dougtebay@gmail.com>
2016-05-18 15:01:17 -0400

    Organize form validator code into prototype functions

--- app/assets/javascripts/services/prescriptionsFormValidator.js
+++ app/assets/javascripts/services/prescriptionsFormValidator.js
@@ -13,176 +13,178 @@ app.services.prescriptionsFormValidator.prototype.checkFormFieldsValidity = func
   {'messageId': '#doctor-last-name-validity-message', 'radioButtonId': '#doctor_type_existing', 'fieldId': '#doctor_last_name'}
   ];
   var miscFormFields = [
-  {'function': lengthValid, 'messageId': '#length-validity-message', 'fieldId': '#prescription_fill_duration'},
-  {'function': refillsValid, 'messageId': '#refills-validity-message', 'fieldId': '#prescription_refills'},
-  {'function': startDateValid, 'messageId': '#start-date-validity-message', 'fieldId': '#prescription_start_date'},
-  {'function': doseSizeValid, 'messageId': '#dose-size-validity-message', 'fieldId': '#prescription_dose_size' }
+  {'functionName': this.lengthValid, 'messageId': '#length-validity-message', 'fieldId': '#prescription_fill_duration'},
+  {'functionName': this.refillsValid, 'messageId': '#refills-validity-message', 'fieldId': '#prescription_refills'},
+  {'functionName': this.startDateValid, 'messageId': '#start-date-validity-message', 'fieldId': '#prescription_start_date'},
+  {'functionName': this.doseSizeValid, 'messageId': '#dose-size-validity-message', 'fieldId': '#prescription_dose_size' }
   ];
-  checkDrugNameValidity(data);
+  this.checkDrugNameValidity(data);
   doctorPharmacyFormFields.forEach(function(formField) {
-    checkDoctorPharmacyFormFieldValidity(formField.messageId, formField.radioButtonId, formField.fieldId);
-  });
+    this.checkDoctorPharmacyFormFieldValidity(formField.messageId, formField.radioButtonId, formField.fieldId);
+  }, this);
   miscFormFields.forEach(function(formField) {
-    checkFormFieldValidity(formField.function, formField.messageId, formField.fieldId);
-  });
+    this.checkMiscFormFieldValidity(formField.functionName, formField.messageId, formField.fieldId);
+  }, this);
+};
+
+app.services.prescriptionsFormValidator.prototype.checkDoctorPharmacyFormFieldValidity = function(validityMessageId, radioButtonId, formFieldId) {
+  function checkDoctorPharmacyFormFieldValidityCallback(elementId, self) {
+    if($(elementId).val().length === 0) {
+      self.showValidityMessage(validityMessageId);
+    } else { self.hideValidityMessage(validityMessageId); }
+  }
+  $(radioButtonId).focus($.proxy(function() {
+    this.hideValidityMessage(validityMessageId);
+  }, this));
+  this.onInputOrChange(formFieldId, checkDoctorPharmacyFormFieldValidityCallback);
+};
+
+app.services.prescriptionsFormValidator.prototype.onInputOrChange = function(elementId, callback) {
+  var self = this;
+  $(elementId).on('input', function() { callback(elementId, self); });
+  $(elementId).on('change', function() { callback(elementId, self); });
+};
+
+app.services.prescriptionsFormValidator.prototype.showValidityMessage = function(messageId) {
+  $(messageId).show();
+};
+
+app.services.prescriptionsFormValidator.prototype.hideValidityMessage = function(messageId) {
+  $(messageId).hide();
+};
+
+app.services.prescriptionsFormValidator.prototype.checkMiscFormFieldValidity = function(checkValidityFunction, validityMessageId, formFieldId) {
+  function checkFormFieldValidityCallback(elementId, self) {
+    if (checkValidityFunction()) { self.hideValidityMessage(validityMessageId); }
+    else { self.showValidityMessage(validityMessageId); }
+  }
+  this.onInputOrChange(formFieldId, checkFormFieldValidityCallback);
 };
 
 app.services.prescriptionsFormValidator.prototype.checkFormValidity = function() {
-  function checkFormValidityCallback() {
-    return formValid() ? disableSubmitButton(false) : disableSubmitButton(true);
+  function checkFormValidityCallback(elementId, self) {
+    return self.formValid() ? self.disableSubmitButton(false) : self.disableSubmitButton(true);
   }
-  onInputOrChange('#form', checkFormValidityCallback);
+  this.onInputOrChange('#form', checkFormValidityCallback);
 };
 
-function checkDrugNameValidity(data) {
-  onInputOrChange('#drug_name', removeDrugNameValidityMessage);
-  $('#drug_name').focusout(function(){
-    if ($(this).val().length > 0) {
-      var drugName = $(this).val();
-      $.ajax({ url: '/drugs', method: 'POST', data: { drug_name: drugName }
-      }).success(function(data) { setDrugNameValidityMessage(data); });
-    } else { removeDrugNameValidityMessage(); }
-  });
-}
+app.services.prescriptionsFormValidator.prototype.checkDrugNameValidity = function(data) {
+  this.onInputOrChange('#drug_name', this.removeDrugNameValidityMessage);
+  $('#drug_name').focusout($.proxy(function() {
+    if ($('#drug_name').val().length > 0) {
+      var drugName = $('#drug_name').val();
+      $.ajax({ url: '/drugs', method: 'POST', data: { drug_name: drugName }, context: this
+      }).success(function(data) {
+        this.setDrugNameValidityMessage(data); });
+    } else { this.removeDrugNameValidityMessage(); }
+  }, this));
+};
 
-function setDrugNameValidityMessage(data) {
+app.services.prescriptionsFormValidator.prototype.removeDrugNameValidityMessage = function() {
+  $('#drug-name-validity-message').empty();
+};
+
+app.services.prescriptionsFormValidator.prototype.setDrugNameValidityMessage = function(data) {
   if(data.validity) { $('#drug-name-validity-message').html('\u2714').show(); }
   else { $('#drug-name-validity-message').html('Invalid name').show(); }
-}
+};
 
-function removeDrugNameValidityMessage() {
-  $('#drug-name-validity-message').empty();
-}
-
-function checkDoctorPharmacyFormFieldValidity(validityMessageId, radioButtonId, formFieldId) {
-  function checkDoctorPharmacyFormFieldValidityCallback() {
-    if($(this).val().length === 0) {
-      showValidityMessage(validityMessageId);
-    } else { hideValidityMessage(validityMessageId); }
-  }
-  $(radioButtonId).focus(function() {
-    hideValidityMessage(validityMessageId);
-  });
-  onInputOrChange(formFieldId, checkDoctorPharmacyFormFieldValidityCallback);
-}
-
-function checkFormFieldValidity(checkValidityFunction, validityMessageId, formFieldId) {
-  function checkFormFieldValidityCallback() {
-    if (checkValidityFunction()) { hideValidityMessage(validityMessageId); }
-    else { showValidityMessage(validityMessageId); }
-  }
-  onInputOrChange(formFieldId, checkFormFieldValidityCallback);
-}
-
-function onInputOrChange(elementId, callback) {
-  $(elementId).on('input', callback);
-  $(elementId).on('change', callback);
-}
-
-function showValidityMessage(messageId) {
-  $(messageId).show();
-}
-
-function hideValidityMessage(messageId) {
-  $(messageId).hide();
-}
-
-function disableSubmitButton(boolean) {
+app.services.prescriptionsFormValidator.prototype.disableSubmitButton = function(boolean) {
   $('#prescription-form-submit').prop('disabled', boolean);
-}
+};
 
 // Check validity of form fields
 
-function drugNameValid() {
+app.services.prescriptionsFormValidator.prototype.drugNameValid = function() {
   return $('#drug-name-validity-message').html() === '\u2714' ||
   !!$('#drug_name').attr('disabled');
-}
+};
 
-function doctorValid() {
-  return existingDoctorSelected() ||
-  newDoctorEntered() ||
-  firstDoctorEntered();
-}
+app.services.prescriptionsFormValidator.prototype.doctorValid = function() {
+  return this.existingDoctorSelected() ||
+  this.newDoctorEntered() ||
+  this.firstDoctorEntered();
+};
 
-function existingDoctorSelected() {
+app.services.prescriptionsFormValidator.prototype.existingDoctorSelected = function() {
   return $('#doctor_type_existing').prop('checked') &&
   $('#doctor_id :selected').val() !== '';
-}
+};
 
-function newDoctorEntered() {
+app.services.prescriptionsFormValidator.prototype.newDoctorEntered = function() {
   return $('#doctor_type_new').prop('checked') &&
   $('#doctor_first_name').val() !== '' &&
   $('#doctor_last_name').val() !== '';
-}
+};
 
-function firstDoctorEntered() {
+app.services.prescriptionsFormValidator.prototype.firstDoctorEntered = function() {
   return !$('#doctor_type_existing').prop('checked') &&
   !$('#doctor_type_new').prop('checked') &&
   $('#doctor_first_name').val() !== '' &&
   $('#doctor_last_name').val() !== '';
-}
+};
 
-function pharmacyValid() {
-  return existingPharmacySelected() ||
-  newPharmacyEntered() ||
-  firstPharmacyEntered();
-}
+app.services.prescriptionsFormValidator.prototype.pharmacyValid = function() {
+  return this.existingPharmacySelected() ||
+  this.newPharmacyEntered() ||
+  this.firstPharmacyEntered();
+};
 
-function existingPharmacySelected() {
+app.services.prescriptionsFormValidator.prototype.existingPharmacySelected = function() {
   return $('#pharmacy_type_existing').prop('checked') &&
   $('#pharmacy_id :selected').val() !== '';
-}
+};
 
-function newPharmacyEntered() {
+app.services.prescriptionsFormValidator.prototype.newPharmacyEntered = function() {
   return $('#pharmacy_type_new').prop('checked') &&
   $('#pharmacy_name').val() !== '' &&
   $('#pharmacy_location').val() !== '';
-}
+};
 
-function firstPharmacyEntered() {
+app.services.prescriptionsFormValidator.prototype.firstPharmacyEntered = function() {
   return !$('#pharmacy_type_existing').prop('checked') &&
   !$('#pharmacy_type_new').prop('checked') &&
   $('#pharmacy_name').val() !== '' &&
   $('#pharmacy_location').val() !== '';
-}
+};
 
-function lengthValid() {
+app.services.prescriptionsFormValidator.prototype.lengthValid = function() {
   return $('#prescription_fill_duration').val() !== '' &&
   parseInt($('#prescription_fill_duration').val()) > 0;
-}
+};
 
-function refillsValid() {
+app.services.prescriptionsFormValidator.prototype.refillsValid = function() {
   return $('#prescription_refills').val() !== '' &&
   parseInt($('#prescription_refills').val()) > -1;
-}
+};
 
-function startDateValid() {
+app.services.prescriptionsFormValidator.prototype.startDateValid = function() {
   var startDate = $('#prescription_start_date').val();
   var dateRegex = /^(?:(20)[0-9]{2})[-.](0[1-9]|1[012])[-.](0[1-9]|[12][0-9]|3[01])$/;
   return dateRegex.test(startDate);
-}
+};
 
-function doseSizeValid() {
+app.services.prescriptionsFormValidator.prototype.doseSizeValid = function() {
   return $('#prescription_dose_size').val() !== '';
-}
+};
 
-function scheduledDosesValid() {
+app.services.prescriptionsFormValidator.prototype.scheduledDosesValid = function() {
   var morning = parseInt($('#scheduled_doses_morning :selected').val());
   var afternoon = parseInt($('#scheduled_doses_afternoon :selected').val());
   var evening = parseInt($('#scheduled_doses_evening :selected').val());
   var bedtime = parseInt($('#scheduled_doses_bedtime :selected').val());
   return morning + afternoon + evening + bedtime > 0 ? true : false;
-}
+};
 
 // Check validity of all fields
 
-function formValid() {
-  return drugNameValid() &&
-  doctorValid() &&
-  pharmacyValid() &&
-  lengthValid() &&
-  refillsValid() &&
-  startDateValid() &&
-  doseSizeValid() &&
-  scheduledDosesValid();
-}
+app.services.prescriptionsFormValidator.prototype.formValid = function() {
+  return this.drugNameValid() &&
+  this.doctorValid() &&
+  this.pharmacyValid() &&
+  this.lengthValid() &&
+  this.refillsValid() &&
+  this.startDateValid() &&
+  this.doseSizeValid() &&
+  this.scheduledDosesValid();
+};

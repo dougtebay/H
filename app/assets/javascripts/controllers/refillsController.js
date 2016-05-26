da5a06ba1d1d42535804054028bd4e0ea6b6c402
dougtebay <dougtebay@gmail.com>
2016-05-25 22:41:38 -0400

    Update overall design

--- app/assets/javascripts/controllers/refillsController.js
+++ app/assets/javascripts/controllers/refillsController.js
@@ -5,34 +5,19 @@ app.controllers.refillsController.prototype.init = function() {
 };
 
 app.controllers.refillsController.prototype.attachListeners = function() {
-  $(document).on('click', '#exp-soon-table form', $.proxy(function(event) {
+  $(document).on('click', '.refill-button-group', $.proxy(function(event) {
     this.update(event);
   }, this));
 };
 
 app.controllers.refillsController.prototype.update = function(event) {
-  event.preventDefault();
-  var rxId = $('#exp-soon-table form').children('.btn').attr('data-rxid');
+  event.stopPropagation();
+  prescriptionId = parseInt($(event.target).closest('div[id]').attr('id'));
   $.ajax({
-    url: '/refills/' + rxId,
+    url: '/refills/' + prescriptionId,
     method: 'PATCH'
   }).success(function(data) {
-    if (data.expSoon && data.prescription.refills > 0) {
-      var expDate = data.expDate;
-      var refills = data.prescription.refills;
-      var $expTd = $('tr[data-rxid='+data.prescription.id+'] td:nth-child(2) span');
-      var $refillsTd = $('tr[data-rxid='+data.prescription.id+'] td:nth-child(3) span');
-      $expTd.fadeOut(200, function() {
-        $(this).text(expDate);
-        $(this).fadeIn(200);
-      });
-      $refillsTd.fadeOut(200, function() {
-        $(this).text(refills);
-        $(this).fadeIn(200);
-      });
-    } else {
-      var $tr = $('tr[data-rxid='+data.prescription.id+']');
-      $tr.hide(300, function(){ $(this).remove(); });
-    }
+    $('.body-partial').remove();
+    $('body').append(data);
   });
 };
--- app/controllers/refills_controller.rb
+++ app/controllers/refills_controller.rb
@@ -4,11 +4,7 @@ class RefillsController < ApplicationController
   def update
     @prescription = Prescription.find(params[:id])
     @prescription.refill
-    if Prescription.ending_within_week.include?(@prescription)
-      @formatted_date = @prescription.format_date(@prescription.end_date)
-      render(json: {prescription: @prescription, expSoon: true, expDate: @formatted_date}, include: [:drug])
-    else
-      render(json: {prescription: @prescription}, include: [:drug])
-    end
+    @user = current_user
+    render :partial => "/users/show", :locals => { user: @user, prescription: @prescription }
   end
 end
